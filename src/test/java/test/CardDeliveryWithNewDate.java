99d9b03fdecd81f7c905a08ab8c7fe745b375585
Andrey Spiridonov <andrejvazapkin@gmail.com>
2022-06-10 01:59:57 +0400

    Allure

--- src/test/java/test/CardDeliveryWithNewDate.java
+++ src/test/java/test/CardDeliveryWithNewDate.java
@@ -2,6 +2,10 @@ package test;
 
 import Data.DataGenerator;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -20,6 +24,15 @@ public class CardDeliveryWithNewDate {
 
     private DataGenerator.UserInfo user = DataGenerator.getUserInfo();
 
+    @BeforeAll
+    static void ssetUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setUp() {
